f71dbf0013067d764c05357c09422b376e426ea3
Ratibor <braveratibor@gmail.com>
2023-02-19 00:24:16 +0300

    Station hardware configuration

---
+++ config/config.h
@@ -0,0 +1,8 @@
+Core - ESP32(40PIN) - Bluetooth/WiFi
+Temperature sensor - DS18B20 - 2 pcs
+CO2 sensor - MH-Z19 (RS485) - 1 pcs
+Humidity sensor - AHT20? (i2c) - 1 pcs
+Coolers - 12V PC - 2 pcs
+Humidifier - Ultrasonic - 1 pcs 220V socket via Relay module
+Push button with fixation - 3 pcs (2*Coolers, Humidator, Light)
+Relay - 3.3V(5V)/220V - 3 pcs
